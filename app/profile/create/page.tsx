560361f626507ee4c2f36cef48e28434091cdf19
Alex <alexmarr2007@gmail.com>
2025-04-23 10:46:48 +0300

    feat: add profile logic and initial component structure

--- app/profile/create/page.tsx
+++ app/profile/create/page.tsx
@@ -1,15 +1,12 @@
 import FormInput from '@/components/form/FormInput'
 import SubmitButton from '@/components/form/SubmitButton'
 import FormContainer from '@/components/form/FormContainer'
-
-const createProfileAction = async (prevState: unknown, formData: FormData) => {
-  'use server'
-  const firstName = formData.get('firstName') as string
-  if (firstName !== 'Test') return { message: 'there was an error...' }
-  return { message: 'Profile Created' }
-}
-
-const CreateProfile = () => {
+import { createProfileAction } from '@/utils/actions'
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+async function CreateProfile() {
+  const user = await currentUser()
+  if (user?.privateMetadata?.hasProfile) redirect('/')
   return (
     <section>
       <h1 className='text-2xl font-semibold mb-8 capitalize'>new user</h1>
